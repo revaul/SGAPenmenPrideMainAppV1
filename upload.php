eb6a77502fb85b2d299db009a4aa1856e351d30c
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-10 11:50:01 +0200

    Revert

--- upload.php
+++ upload.php
@@ -44,9 +44,71 @@ ini_set('max_execution_time', 300);
                          if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-//Blank Insert Begin
 $insertdaystmt = "INSERT INTO `ppv0008003`.`scanner` (`EventID`, `Scanner`) VALUES (".$eventid.", '0000001');";
 	  echo $insertdaystmt;
+	  echo "<br>";
+            if ($conn->query($insertdaystmt) === TRUE) {
+            } else {
+                echo "Error: " . $insertdaystmt . "<br>" . $conn->error;
+            }
+// Output one line until end-of-file
+while(!feof($myfile)) {
+  $line = fgets($myfile);
+  $linelength = strlen($line);
+ 
+  if($linelength==18){
+	  $lineremove = str_replace(array("\r", "\n"), '', $line);
+	  $insertdaystmt = "INSERT INTO `ppv0008003`.`scanner` (`EventID`, `Scanner`) VALUES (".$eventid.", '".$lineremove."');";
+	  echo $insertdaystmt;
+	  echo "<br>";
+            if ($conn->query($insertdaystmt) === TRUE) {
+            } else {
+                echo "Error: " . $insertdaystmt . "<br>" . $conn->error;
+            }
+  }
+}
+fclose($myfile);
+if (!unlink($fileurl))
+  {
+  echo "Error deleting". $fileurl;
+  }
+else
+  {
+  echo "Deleted".$fileurl;
+  }
+   }
+?>
+
+      <h2>Penmen Pride File Uploader</h2>
+      <form action="" method="POST" enctype="multipart/form-data">
+	  <p><label>Event: </label>
+<select name="eventid">
+<option value="">Select Event</option>
+<?php 
+require 'mysqlkeys.php';
+$con=mysqli_connect($host , $user , $password , $dbname );
+if (mysqli_connect_errno()){
+	echo "Failed to connect:".mysqli_connect_errno();
+	}
+$query = "SELECT * FROM ppv0008003.eventnames order by eventnames.EventDate DESC;";
+$results=mysqli_query($con, $query);
+foreach ($results as $HostName){
+?>
+<option value="<?php echo $HostName["EventID"]; ?>"><?php echo $HostName["EventID"]." - ".$HostName["EventDate"]." - ". $HostName["EventName"]; ?></option>
+<?php
+}
+?>
+</select></p>
+          <p><label>File: </label>
+              <input type="file" name="image" /></p>
+         <input type="submit"/>
+      </form>
+        </div>
+        <div class="col-sm-2">
+          </div>
+            </div>
+</div>
+      <?php include "template/bottom.php" ?>;
 	  echo "<br>";
             if ($conn->query($insertdaystmt) === TRUE) {
             } else {
