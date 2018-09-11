2c2cd8c9124877160dd5ea8904eb1da8ca6e773e
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-12 00:12:25 +0200

    Upload fix

--- upload.php
+++ upload.php
@@ -6,6 +6,7 @@
           </div>
     <div class="col-sm-8">
 <?php
+	    //Upload Begin
 $fileurl="TEST";
    if(isset($_FILES['image'])){
       $errors= array();
@@ -33,6 +34,7 @@ $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
 	  $fileurl = "uploads/".$file_name;
 	  $myopenfile;
    }
+	    //Upload End
    
    if($fileurl!="TEST"){
 	   $eventid= $_POST["eventid"];
@@ -44,9 +46,70 @@ ini_set('max_execution_time', 300);
                          if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
 $insertdaystmt = "INSERT INTO `ppv0008003`.`scanner` (`EventID`, `Scanner`) VALUES (".$eventid.", '0000001');";
 	  echo $insertdaystmt;
+	  echo "<br>";
+            if ($conn->query($insertdaystmt) === TRUE) {
+            } else {
+                echo "Error: " . $insertdaystmt . "<br>" . $conn->error;
+            }
+// Output one line until end-of-file
+while(!feof($myfile)) {
+  $line = fgets($myfile);
+  $linelength = strlen($line);
+  if($linelength==17){
+	  $lineremove = str_replace(array("\r", "\n"), '', $line);
+	  $insertdaystmt = "INSERT INTO `ppv0008003`.`scanner` (`EventID`, `Scanner`) VALUES (".$eventid.", '".$lineremove."');";
+	  echo $insertdaystmt;
+	  echo "<br>";
+            if ($conn->query($insertdaystmt) === TRUE) {
+            } else {
+                echo "Error: " . $insertdaystmt . "<br>" . $conn->error;
+            }
+  }
+}
+fclose($myfile);
+if (!unlink($fileurl))
+  {
+  echo "Error deleting". $fileurl;
+  }
+else
+  {
+  echo "Deleted".$fileurl;
+  }
+   }
+?>
+
+      <h2>Penmen Pride File Uploader</h2>
+      <form action="" method="POST" enctype="multipart/form-data">
+	  <p><label>Event: </label>
+<select name="eventid">
+<option value="">Select Event</option>
+<?php 
+require 'mysqlkeys.php';
+$con=mysqli_connect($host , $user , $password , $dbname );
+if (mysqli_connect_errno()){
+	echo "Failed to connect:".mysqli_connect_errno();
+	}
+$query = "SELECT * FROM ppv0008003.eventnames order by eventnames.EventDate DESC;";
+$results=mysqli_query($con, $query);
+foreach ($results as $HostName){
+?>
+<option value="<?php echo $HostName["EventID"]; ?>"><?php echo $HostName["EventID"]." - ".$HostName["EventDate"]." - ". $HostName["EventName"]; ?></option>
+<?php
+}
+?>
+</select></p>
+          <p><label>File: </label>
+              <input type="file" name="image" /></p>
+         <input type="submit"/>
+      </form>
+        </div>
+        <div class="col-sm-2">
+          </div>
+            </div>
+</div>
+      <?php include "template/bottom.php" ?>;
 	  echo "<br>";
             if ($conn->query($insertdaystmt) === TRUE) {
             } else {
