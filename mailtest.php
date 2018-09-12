e58ebf502d41699eaf6736fdae90eddba6e22dfa
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-12 12:11:06 +0200

    Mail test

---
+++ mailtest.php
@@ -0,0 +1,26 @@
+<?php 
+require_once "Mail.php";
+require 'mysqlkeys.php';
+require "login/loginheader.php";
+$from = $mailfrom;
+$to = "Ramona Recipient <recipient@example.com>";
+$subject = "Hi!";
+$body = "Hi,\n\nHow are you?";
+$host = $mailhost;
+$username = $mailusername;
+$password = $mailpassword;
+$headers = array ('From' => $from,
+  'To' => $to,
+  'Subject' => $subject);
+$smtp = Mail::factory('smtp',
+  array ('host' => $host,
+    'auth' => true,
+    'username' => $username,
+    'password' => $password));
+$mail = $smtp->send($to, $headers, $body);
+if (PEAR::isError($mail)) {
+  echo("<p>" . $mail->getMessage() . "</p>");
+ } else {
+  echo("<p>Message successfully sent!</p>");
+ }
+ ?>
