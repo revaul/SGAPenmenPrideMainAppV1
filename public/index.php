cba1432fbfca63bb7894b21ac5702dbcf749241f
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-13 01:29:16 +0200

    testtemp

--- public/index.php
+++ public/index.php
@@ -1,7 +1,15 @@
-<html>
-<body>
-
-Welcome <?php echo $_GET["id"]; ?>
-
-</body>
-</html>
+<?php include "./template/publictop.php"; ?>
+<div id="about" class="container-fluid">
+      <div class="row">
+          <div class="col-sm-2">
+          </div>
+    <div class="col-sm-8">
+      
+      
+        <h2> All Keys Created $_GET['id'] </h2>
+	    </div>
+    <div class="col-sm-1">
+    </div>
+  </div>
+</div>
+<?php include "template/bottom.php" ?>
