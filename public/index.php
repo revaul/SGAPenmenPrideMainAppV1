1350779a18f2251cf248ddc1890fc6bab911338f
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-14 14:11:48 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -75,51 +75,40 @@ else{
     success : function(data){
       console.log(data);
 
-      var userid = [];
-      var facebook_follower = [];
-      var twitter_follower = [];
-      var googleplus_follower = [];
+      var PubSemester = [];
+      var totpoints = [];
+      var totevents = [];
 
       for(var i in data) {
-        userid.push("UserID " + data[i].userid);
-        facebook_follower.push(data[i].facebook);
-        twitter_follower.push(data[i].twitter);
-        googleplus_follower.push(data[i].googleplus);
+        PubSemester.push("PubSemester " + data[i].PubSemester);
+        totpoints.push(data[i].totpoints);
+        totevents.push(data[i].totevents);
       }
 
       var chartdata = {
         labels: userid,
         datasets: [
           {
-            label: "facebook",
+            label: "totpoints",
             fill: false,
             lineTension: 0.1,
             backgroundColor: "rgba(59, 89, 152, 0.75)",
             borderColor: "rgba(59, 89, 152, 1)",
             pointHoverBackgroundColor: "rgba(59, 89, 152, 1)",
             pointHoverBorderColor: "rgba(59, 89, 152, 1)",
-            data: facebook_follower
+            data: totpoints
           },
           {
-            label: "twitter",
+            label: "totevents",
             fill: false,
             lineTension: 0.1,
             backgroundColor: "rgba(29, 202, 255, 0.75)",
             borderColor: "rgba(29, 202, 255, 1)",
             pointHoverBackgroundColor: "rgba(29, 202, 255, 1)",
             pointHoverBorderColor: "rgba(29, 202, 255, 1)",
-            data: twitter_follower
-          },
-          {
-            label: "googleplus",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(211, 72, 54, 0.75)",
-            borderColor: "rgba(211, 72, 54, 1)",
-            pointHoverBackgroundColor: "rgba(211, 72, 54, 1)",
-            pointHoverBorderColor: "rgba(211, 72, 54, 1)",
-            data: googleplus_follower
+            data: totevents
           }
+          
         ]
       };
 
