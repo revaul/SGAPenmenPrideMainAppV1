b785ab67597fd57b82e928b7fa9d8be24d3c226b
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-14 14:23:58 +0200

    Update chart1data.php

--- public/chart1data.php
+++ public/chart1data.php
@@ -14,9 +14,26 @@ $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if(!$mysqli){
   die("Connection failed: " . $mysqli->error);
 }
-
+$idnumnotsafe = $_GET["id"];
+$idnumlenchecked;
+if (strlen($idnumnotsafe)!=32){
+$idnumlenchecked="9bce422057d28895fa132da3d68f065b";
+}
+else{
+$idnumlenchecked = $idnumnotsafe;
+}
+$idnumlower = strtolower($idnumlenchecked);
+$vowels = array("select", "insert", "update", "delete", "execute", "show", "view", "create", "alter", "references", "index", "create", "view", " ", "routine", "event", "drop", "trigger", "grant", "option", "temporary", "table", "lock", ";" ,"and", "or", ",", "echo", "$");
+$idnocommands = str_replace($vowels,"",$idnumlower);
+$idclean;
+if (strlen($idnocommands)!=32){
+$idclean="9bce422057d28895fa132da3d68f065b";
+}
+else{
+$idclean = $idnocommands;
+}
 //query to get data from the table
-$query = sprintf("SELECT PubSemester, totpoints, totevents FROM ppv0008004.pointspersemesterordered where Pubrandomkeycol='42e2725c64bd6fd33842d6dabfc49d8f'");
+$query = sprintf("SELECT PubSemester, totpoints, totevents FROM ppv0008004.pointspersemesterordered where Pubrandomkeycol=".$idclean.";");
 
 //execute query
 $result = $mysqli->query($query);
