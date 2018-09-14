2d689de4c553a8f0c43a96c40866f242d6ab2381
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-14 12:50:56 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -54,54 +54,140 @@ else{
 }
 	   ?>
 <div id="about" class="container-fluid">
-      <div class="row">
-              <div class="col-sm-1">
-    </div>
-    <div class="col-sm-10">
-	    <h2>Welcome <?php echo $name; ?>,</h2>
-	    <h3>This semester you have earned <?php echo $totpoints; ?> point<?php if ($totpoints!=1){echo "s";}?> and attended <?php echo $totevents; ?> event<?php if ($totevents!=1){echo "s";}?>.</h3>
-<table class="table table-striped">
-      <col width="10%">
-  <col width="35%">
-      <col width="15%">
-  <col width="10%">
-      <col width="10%">
-  <col width="20%">
-<tr>
-<th>Event Date
-</th>
-<th>Event Name
-</th>
-<th>Event Host
-</th>
-<th>Semester
-</th>
-<th>Points
-</th>
-</tr>
+    <div class="row">
+        <div class="col-sm-1">
+        </div>
+        <div class="col-sm-10">
+            <h2>Welcome
+                <?php echo $name; ?>,</h2>
+            <h3>This semester you have earned
+                <?php echo $totpoints; ?> point
+                <?php if ($totpoints!=1){echo "s";}?> and attended
+                <?php echo $totevents; ?> event
+                <?php if ($totevents!=1){echo "s";}?>.</h3>
+            //Start Paste of Chart
+            <div id="canvas-holder" style="width:40%">
+                <canvas id="chart-area"></canvas>
+            </div>
 
-<?php
+            <script>
+                var config = {
+                    type: 'doughnut',
+                    data: {
+                        datasets: [{
+                            data: [
+                                1,
+                                2,
+                                3,
+                                4,
+                                5,
+                            ],
+                            backgroundColor: [
+                                window.chartColors.red,
+                                window.chartColors.orange,
+                                window.chartColors.yellow,
+                                window.chartColors.green,
+                                window.chartColors.blue,
+                            ],
+                            label: 'Dataset 1'
+                        }],
+                        labels: [
+                            'Red',
+                            'Orange',
+                            'Yellow',
+                            'Green',
+                            'Blue'
+                        ]
+                    },
+                    options: {
+                        responsive: true,
+                        legend: {
+                            position: 'top',
+                        },
+                        title: {
+                            display: true,
+                            text: 'Chart.js Doughnut Chart'
+                        },
+                        animation: {
+                            animateScale: true,
+                            animateRotate: true
+                        }
+                    }
+                };
+
+                window.onload = function() {
+                    var ctx = document.getElementById('chart-area').getContext('2d');
+                    window.myDoughnut = new Chart(ctx, config);
+                };
+
+                var colorNames = Object.keys(window.chartColors);
+
+                    
+
+            </script>
+            //End Paste of Chart
+
+            <table class="table table-striped">
+                <col width="10%">
+                <col width="35%">
+                <col width="15%">
+                <col width="10%">
+                <col width="10%">
+                <col width="20%">
+                <tr>
+                    <th>Event Date
+                    </th>
+                    <th>Event Name
+                    </th>
+                    <th>Event Host
+                    </th>
+                    <th>Semester
+                    </th>
+                    <th>Points
+                    </th>
+                </tr>
+
+                <?php
 $sql = "SELECT * FROM ppv0008004.pubscanner where Pubrandomkeycol='".$idclean."' order by PubEventDate desc;;";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-       ?><tr><td> <?php echo $row["PubEventDate"];
-	   ?></td><td><?php echo $row["PubEventName"];
-	   ?></td><td><?php echo $row["PubEventHost"];
-	   ?></td><td><?php echo $row["PubSemester"];
-	   ?></td><td><?php echo $row["PubPointValue"];
-	   ?></td></tr><?php
+       ?>
+                    <tr>
+                        <td>
+                            <?php echo $row["PubEventDate"];
+	   ?>
+                        </td>
+                        <td>
+                            <?php echo $row["PubEventName"];
+	   ?>
+                        </td>
+                        <td>
+                            <?php echo $row["PubEventHost"];
+	   ?>
+                        </td>
+                        <td>
+                            <?php echo $row["PubSemester"];
+	   ?>
+                        </td>
+                        <td>
+                            <?php echo $row["PubPointValue"];
+	   ?>
+                        </td>
+                    </tr>
+                    <?php
     }
 } else {
  
 }
 $conn->close();
 ?>
-</table>
+            </table>
         </div>
-    <div class="col-sm-1">
+        <div class="col-sm-1">
+        </div>
     </div>
-  </div>
 </div>
 <?php include '../template/bottom.php' ?>
+
