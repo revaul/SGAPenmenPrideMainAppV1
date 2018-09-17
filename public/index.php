595ed8d66ae422d1272efbd2a987a9eb5c880724
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-17 15:11:09 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -233,7 +233,7 @@ $url = $protocol . $_SERVER['HTTP_HOST'];
 	<div class="row">
         <div class="col-sm-1">
         </div>
-        <div class="col-sm-10">
+        <div class="col-sm-10 graphbox">
 		<h3>You Have Attended the Following Events:</h3>
             <table class="table table-striped">
                 <col width="10%">
