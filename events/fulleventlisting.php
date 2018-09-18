0c26e871511222b1c36ef2aacc6c778a5a60d611
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-18 18:30:10 +0200

    Update and rename fulleventlisting.php to events/fulleventlisting.php

--- events/fulleventlisting.php
+++ events/fulleventlisting.php
@@ -1,5 +1,5 @@
-<?php require "login/loginheader.php"; ?>
-<?php include "template/top.php"; ?>
+<?php require "../login/loginheader.php"; ?>
+<?php include "../template/top.php"; ?>
 <div id="about" class="container-fluid">
       <div class="row">
               <div class="col-sm-1">
@@ -29,7 +29,7 @@
 </tr>
 
 <?php
-require 'mysqlkeys.php';
+require '../mysqlkeys.php';
 // Create connection
 $conn = new mysqli($host, $user, $password, $dbname);
 // Check connection
@@ -62,4 +62,4 @@ $conn->close();
     </div>
   </div>
 </div>
-<?php include "template/bottom.php" ?>
+<?php include "../template/bottom.php" ?>
