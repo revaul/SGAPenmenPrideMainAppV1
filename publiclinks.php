4f15068037f11db21ce4ff6cd3f8053c4725e599
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-18 17:12:52 +0200

    Update publiclinks.php

--- publiclinks.php
+++ publiclinks.php
@@ -6,6 +6,12 @@
     </div>
     <div class="col-sm-10">
         <h2>Penmen Pride Public Link Listing</h2>
+	      </div>
+	      <div class "col-sm-1">
+	      </div>
+	      <div class="row">
+		      <div class="row">
+			      <div class="col-sm-12">
 <table class="table table-striped">
       <col width="35%">
   <col width="10%">
@@ -59,8 +65,7 @@ $conn->close();
 ?>
 </table>
         </div>
-    <div class="col-sm-1">
-    </div>
+    
   </div>
 </div>
 <?php include "template/bottom.php" ?>
