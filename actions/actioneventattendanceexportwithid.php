48e46d24af2bd911b98d151c11e90a744db499cd
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-20 13:45:32 +0200

    Update actioneventattendanceexportwithid.php

--- actions/actioneventattendanceexportwithid.php
+++ actions/actioneventattendanceexportwithid.php
@@ -5,7 +5,7 @@
                     require '../mysqlkeys.php';
 					// output headers so that the file is downloaded rather than displayed
 header('Content-type: text/csv; charset=utf-8');
-header('Content-Disposition: attachment; filename="EventAttendanceList.csv"');
+header('Content-Disposition: attachment; filename="EventAttendanceListWithStudentIDs.csv"');
  
 // do not cache the file
 header('Pragma: no-cache');
@@ -15,13 +15,13 @@ header('Expires: 0');
 $output = fopen('php://output', 'w');
  
 // send the column headers
-fputcsv($output, array('Email', 'First Name', 'Last Name'));
+fputcsv($output, array('Email', 'First Name', 'Last Name', 'Student ID'));
  
 // Open the connection
 $link = mysqli_connect($host, $user, $password, $dbname);
  
 //query the database
-$query = "select studentlisting.EmailAddress, studentlisting.StudentFirstName, studentlisting.StudentLastName from scanner join eventnames on eventnames.EventID=scanner.EventID join datestosemesters on datestosemesters.Date=eventnames.EventDate join semester on datestosemesters.Semester=semester.Semester inner join scannerlinkouter on scanner.Scanner=scannerlinkouter.Scanner inner join scannerlinkinner on scannerlinkouter.ScannerLinkAddressOuter=scannerlinkinner.ScannerLinkAddress inner join studentlisting on scannerlinkinner.Email=studentlisting.EmailAddress where eventnames.EventID = ".$eventid." AND studentlisting.EmailAddress!='sga@snhu.edu' order by studentlisting.EmailAddress;";
+$query = "select studentlisting.EmailAddress, studentlisting.StudentFirstName, studentlisting.StudentLastName, scannerlinkouter.ScannerLinkAddressOuter from scanner join eventnames on eventnames.EventID=scanner.EventID join datestosemesters on datestosemesters.Date=eventnames.EventDate join semester on datestosemesters.Semester=semester.Semester inner join scannerlinkouter on scanner.Scanner=scannerlinkouter.Scanner inner join scannerlinkinner on scannerlinkouter.ScannerLinkAddressOuter=scannerlinkinner.ScannerLinkAddress inner join studentlisting on scannerlinkinner.Email=studentlisting.EmailAddress where eventnames.EventID = ".$eventid." AND studentlisting.EmailAddress!='sga@snhu.edu' order by studentlisting.EmailAddress;";
  
 if ($rows = mysqli_query($link, $query))
 {
