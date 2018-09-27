8cda9fbfef641fa900dde956d403ae4b6da1e190
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-27 23:08:49 +0200

    file structure

--- emailsystem/mailtextentry.php
+++ emailsystem/mailtextentry.php
@@ -1,5 +1,5 @@
-<?php require "login/loginheader.php"; ?>
-<?php include "template/top.php"; ?>
+<?php require "../login/loginheader.php"; ?>
+<?php include "../template/top.php"; ?>
 <div id="about" class="container-fluid">
   <div class="row">
     <div class="col-sm-8">
@@ -143,4 +143,4 @@
   </div>
 </div>
 </div>
-<?php include "template/bottom.php" ?>
+<?php include "../template/bottom.php" ?>
