0f24ae07ee5058fecfcdff7744f8a230eeb13566
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-27 18:47:46 +0200

    delete emoticons

--- emailsystem/plugin/tinymce/init-tinymce.js
+++ emailsystem/plugin/tinymce/init-tinymce.js
@@ -21,18 +21,10 @@ tinymce.init({
 	],
 
 	/* toolbar */
-	toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons",
+	toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor",
 
 	/* style */
 	style_formats: [
-		{title: "Headers", items: [
-			{title: "Header 1", format: "h1"},
-			{title: "Header 2", format: "h2"},
-			{title: "Header 3", format: "h3"},
-			{title: "Header 4", format: "h4"},
-			{title: "Header 5", format: "h5"},
-			{title: "Header 6", format: "h6"}
-		]},
 		{title: "Inline", items: [
 			{title: "Bold", icon: "bold", format: "bold"},
 			{title: "Italic", icon: "italic", format: "italic"},
