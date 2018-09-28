7be491b1e1373e1982114e366e781d8a5a4eef94
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-28 17:20:09 +0200

    10 to 12

--- emailsystem/viewpendingemails.php
+++ emailsystem/viewpendingemails.php
@@ -2,9 +2,8 @@
 <?php include "../template/top.php"; ?>
 <div id="about" class="container-fluid">
       <div class="row">
-              <div class="col-sm-1">
-    </div>
-    <div class="col-sm-10">
+
+    <div class="col-sm-12">
         <h2>Pending Emails To Be Sent</h2>
 <table class="table table-striped">
       <col width="14%">
@@ -53,8 +52,7 @@ $conn->close();
 ?>
 </table>
         </div>
-    <div class="col-sm-1">
-    </div>
+    
   </div>
 </div>
 <?php include "../template/bottom.php" ?>
