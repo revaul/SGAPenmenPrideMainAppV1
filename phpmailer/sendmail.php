7a517a87619d092ff5272254b2db2648f849fb3b
Ryan Evaul <ryan.evaul@snhu.edu>
2018-09-28 02:35:34 +0200

    All emails

--- phpmailer/sendmail.php
+++ phpmailer/sendmail.php
@@ -20,7 +20,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM ppv0008003.emailmailmergeexport limit 1;";
+$sql = "SELECT * FROM ppv0008003.emailmailmergeexport;";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
