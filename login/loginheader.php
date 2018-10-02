dbcdd5038edd820ee197310c98bb111bd3c410e4
Ryan Evaul <ryan.evaul@snhu.edu>
2018-10-02 15:35:19 +0200

    Fix Main_login Redirect for Subdirectories

--- login/loginheader.php
+++ login/loginheader.php
@@ -2,5 +2,8 @@
 //PUT THIS HEADER ON TOP OF EACH UNIQUE PAGE
 session_start();
 if (!isset($_SESSION['username'])) {
-    return header("location:login/main_login.php");
+  $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+
+  $url = $protocol . $_SERVER['HTTP_HOST'] . "/login/main_login.php";
+    return header("location:".$url);
 }
