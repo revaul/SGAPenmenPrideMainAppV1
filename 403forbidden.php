72ce30dd089baca080ff7760c68a27519a640e0b
Ryan Evaul <ryan.evaul@snhu.edu>
2018-10-03 20:50:07 +0200

    403 better

--- 403forbidden.php
+++ 403forbidden.php
@@ -7,8 +7,8 @@ require 'template/top.php';
         <div class="col-sm-1">
         </div>
         <div class="col-sm-10">
-        <h2 style="text-align: center;"> Sorry! </h2>
-          <p> You do not have permission to access this page. If you believe this to be an error please contact the Executive Officer of SGA for permisions. <br>
+        <h1 style="text-align: center;"> Sorry! </h1>
+          <p style="text-align: center;"><br>You do not have permission to access this page. If you believe this to be an error, please contact the Executive Officer of SGA for permisions. <br> <br>
         Best,<br>
       Ryan C. Evaul</p>
         </div>
@@ -16,4 +16,4 @@ require 'template/top.php';
         </div>
       </div>
     </div>
-    <?php include '../template/bottom.php' ?>
+    <?php include 'template/bottom.php' ?>
