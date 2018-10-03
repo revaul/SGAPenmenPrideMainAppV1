65109ff7bc8e8dbd91eef0b55536bf39fd35e39c
Ryan Evaul <ryan.evaul@snhu.edu>
2018-10-03 18:43:12 +0200

    Cheers

---
+++ login/dbconf.php
@@ -0,0 +1,11 @@
+<?php
+//DATABASE CONNECTION VARIABLES
+require '../mysqlkeys.php';
+ // Host name
+$username = $user; // Mysql username
+ // Mysql password
+$db_name = $logindbname; // Database name
+//DO NOT CHANGE BELOW THIS LINE UNLESS YOU CHANGE THE NAMES OF THE MEMBERS AND LOGINATTEMPTS TABLES
+$tbl_prefix = ""; //***PLANNED FEATURE, LEAVE VALUE BLANK FOR NOW*** Prefix for all database tables
+$tbl_members = $tbl_prefix."members";
+$tbl_attempts = $tbl_prefix."loginAttempts";
