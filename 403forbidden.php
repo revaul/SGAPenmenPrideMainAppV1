a6bf82cd7bf35f72292908220e89e8f4b9c362f2
Ryan Evaul <ryan.evaul@snhu.edu>
2018-10-03 20:46:15 +0200

    403 message

--- 403forbidden.php
+++ 403forbidden.php
@@ -7,7 +7,10 @@ require 'template/top.php';
         <div class="col-sm-1">
         </div>
         <div class="col-sm-10">
-        <h2> Error - You do not have permissions. </h2>
+        <h2 style="text-align: center;"> Sorry, <br>
+          You do not have permission to access this page. If you believe this to be an error please contact the Executive Officer of SGA for permisions. <br>
+        Best,<br>
+      Ryan C. Evaul</h2>
         </div>
         <div class="col-sm-1">
         </div>
