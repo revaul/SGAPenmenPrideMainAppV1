7e814b897a99fb8c1770fbfc9e2eb837f4dc058f
Ryan Evaul <ryan.evaul@snhu.edu>
2018-10-04 16:36:09 +0200

    executive officer create account

--- login/config.php
+++ login/config.php
@@ -29,7 +29,7 @@ $smtp_security = $mailsecurity;//ssl, tls or ''
 $verifymsg = 'Click this link to verify your new account!'; //Verify email message
 $active_email = 'Your new account is now active! Click this link to log in!';//Active email message
 //LOGIN FORM RESPONSE MESSAGES/ERRORS
-$signupthanks = 'Thank you for signing up! You will receive an email shortly confirming the verification of your account.';
+$signupthanks = 'Your account has been requested. In order to continue, the executive officer will have to approve your account.';
 $activemsg = 'Your account has been verified! You may now login at <br><a href="'.$signin_url.'">'.$signin_url.'</a>';
 //DO NOT TOUCH BELOW THIS LINE
 //Unsets $admin_email based on various conditions (left blank, not valid email, etc)
