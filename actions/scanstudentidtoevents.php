d834a7b8bb87bf238bb13d7f85698992d607f784
Ryan Evaul <ryan.evaul@snhu.edu>
2019-01-28 18:36:51 -0500

    ScanID

--- actions/scanstudentidtoevents.php
+++ actions/scanstudentidtoevents.php
@@ -6,30 +6,31 @@
               <div class="col-sm-1">
     </div>
     <div class="col-sm-10">
-      <h2 style="text-align:center">Scan ID</h2>
+      <?php
+      // TODO: Post Field Verification
+      $scannertype= $_POST["scannertype"];
+      $eventname= $_POST["event"];
+      $bothpass=TRUE;
+      if (!is_numeric($scannertype)) {
+              $bothpass=FALSE;
+          }
+          else{
+
+          }
+          if (!is_numeric($eventname)) {
+                  $bothpass=FALSE;
+              }
+              else{
+
+              }
+              if($bothpass){
+                  ?>
+      <h2 style="text-align:center"><?php if($scannertype==3){echo "Enter Student ID (Including Zero)"; } else { echo "Scan ID"; } ?></h2>
       <form action="actionscanstudentidtoevents.php" method="post">
-<?php
-// TODO: Post Field Verification
-$scannertype= $_POST["scannertype"];
-$eventname= $_POST["event"];
-$bothpass=TRUE;
-if (!is_numeric($scannertype)) {
-        $bothpass=FALSE;
-    }
-    else{
 
-    }
-    if (!is_numeric($eventname)) {
-            $bothpass=FALSE;
-        }
-        else{
-
-        }
-if($bothpass){
-    ?>
 <input type="hidden" name="eventname" value="<?php echo $eventname; ?>">
 <input type="hidden" name="scannertype" value="<?php echo $scannertype; ?>">
-<input type="text" style="display:block;margin:auto;text-align:center;height:.01px;width:.01px" name="scanner" autofocus>
+<input type="text" style="display:block;margin:auto;text-align:center;<?php if($scannertype==3){} else{ ?>height:.01px;width:.01px<?php } ?>" name="scanner" autofocus>
 <br>
 <input style="display:block;margin:auto;text-align:center" type="submit">
     </form>
