5c7cf8c071403149b69d6573c73b06963f74ac01
Ryan Evaul <ryan.evaul@snhu.edu>
2019-03-23 17:01:38 -0400

    Adding Student ID Number to Possible Scanning

--- events/chooseeventtoscanat.php
+++ events/chooseeventtoscanat.php
@@ -52,8 +52,8 @@ $query;
   <p><label>Choose Scanning Method:   </label><select name="scannertype">
 <option value="1">Scanning Via iPad</option>
 <!--<option value="1">Scanning Via Laptop USB Scanner</option>
-<option value="2">Scanning Via Manually Entering Email</option>
-<option value="3">Scanning Via Manually Entering Student ID Number</option>-->
+<option value="2">Scanning Via Manually Entering Email</option>-->
+<option value="3">Manually Enter Student ID Number</option>-->
   </select>
   </p>
 <?php } ?>
