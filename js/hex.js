050fcb410f58fe0690c3534dc222bec8b1f9a119
Ryan Evaul <ryan.evaul@snhu.edu>
2019-03-24 23:26:01 -0400

    hex layout update

--- js/hex.js
+++ js/hex.js
@@ -3,37 +3,87 @@ jQuery(function($) {
 	// QWERTY Text Input
 	// The bottom of this file is where the autocomplete extension is added
 	// ********************
-	$('#text').keyboard({ layout: 'qwerty' });
-  $('#myusername').keyboard({ layout: 'qwerty' });
-  $('#mypassword').keyboard({ layout: 'qwerty' });
-
-	$('.version').html( '(v' + $('#text').getkeyboard().version + ')' );
+	//$('#text').keyboard({ layout: 'qwerty' });
+  //$('#myusername').keyboard({ layout: 'qwerty' });
+  //$('#mypassword').keyboard({ layout: 'qwerty' });
+  $('#mypassword').keyboard({
+  	usePreview: true, // disabled for contenteditable
+  	useCombos: false,
+  	autoAccept: true,
+  	layout: 'custom',
+  	customLayout: {
+  		'normal': [
+  			'` 1 2 3 4 5 6 7 8 9 0 - = {b}',
+  			'q w e r t y u i o p [ ] \\',
+  			'a s d f g h j k l ; \' {accept}',
+  			'{shift} z x c v b n m , . / {shift}',
+  			'{accept} {space} {left} {right}'
+  		],
+  		'shift': [
+  			'~ ! @ # $ % ^ & * ( ) _ + {b}',
+  			'Q W E R T Y U I O P { } |',
+  			'A S D F G H J K L : " {accept}',
+  			'{shift} Z X C V B N M < > ? {shift}',
+  			'{accept} {space} {left} {right}'
+  		]
+  	}
+  })
+  .addTyping();
+  $('#myusername').keyboard({
+  	usePreview: true, // disabled for contenteditable
+  	useCombos: false,
+  	autoAccept: true,
+  	layout: 'custom',
+  	customLayout: {
+  		'normal': [
+  			'` 1 2 3 4 5 6 7 8 9 0 - = {b}',
+  			'q w e r t y u i o p [ ] \\',
+  			'a s d f g h j k l ; \' {accept}',
+  			'{shift} z x c v b n m , . / {shift}',
+  			'{accept} {space} {left} {right}'
+  		],
+  		'shift': [
+  			'~ ! @ # $ % ^ & * ( ) _ + {b}',
+  			'Q W E R T Y U I O P { } |',
+  			'A S D F G H J K L : " {accept}',
+  			'{shift} Z X C V B N M < > ? {shift}',
+  			'{accept} {space} {left} {right}'
+  		]
+  	}
+  })
+  .addTyping();
+	//$('.version').html( '(v' + $('#text').getkeyboard().version + ')' );
 
 
 	// Custom: Hex
 	// ********************
-	$('#hex').keyboard({
-		layout: 'custom',
-		customLayout: {
-			'normal' : [
-				'1 2 3',
-				'4 5 6',
-				'7 8 9',
-				'0',
-				'{bksp} {enter}'
-			]
-		},
-		maxLength : 7,
-		restrictInput : true, // Prevent keys not in the displayed keyboard from being typed in
-		// include lower case characters (added v1.25.7)
-		restrictInclude : 'a b c d e f',
-		useCombos : false, // don't want A+E to become a ligature
-		acceptValid: true,
-		validate: function(keyboard, value, isClosing){
-			// only make valid if input is 6 characters in length
-			return value.length === 7;
-		}
-	});
+  $('#text')
+  	.keyboard({
+  		layout: 'custom',
+  		customLayout: {
+  			'normal' : [
+  				'1 2 3',
+  				'4 5 6',
+  				'7 8 9',
+  				'0',
+  				'{bksp} {a} {c}'
+  			]
+  		},
+  		maxLength : 7,
+  		// Prevent keys not in the displayed keyboard from being typed in
+  		restrictInput : true,
+  		// include lower case characters (added v1.25.7)
+  		restrictInclude : 'a b c d e f',
+  		// don't use combos or A+E could become a ligature
+  		useCombos : false,
+  		// activate the "validate" callback function
+  		acceptValid : true,
+  		validate : function(keyboard, value, isClosing){
+  			// only make valid if input is 6 characters in length
+  			return value.length === 7;
+  		}
+  	})
+  	.addTyping();
 
 
 // ********************
