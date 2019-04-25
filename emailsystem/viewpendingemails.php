1c91600843756c48c8510a9e14286c2e1fe9dc60
Ryan Evaul <ryan.evaul@snhu.edu>
2019-04-25 02:15:53 -0400

    emails 2

--- emailsystem/viewpendingemails.php
+++ emailsystem/viewpendingemails.php
@@ -28,7 +28,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM ppv0008003.PendingEmails;";
+$sql = "SELECT * FROM ppv0008003.emailsystem;";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
