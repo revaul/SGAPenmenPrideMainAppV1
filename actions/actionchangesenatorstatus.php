a0a91e757b67d2ff4399ba4c592a1310442478f3
Ryan Evaul <ryan.evaul@snhu.edu>
2019-09-08 12:17:25 -0400

    Set User Status

--- actions/actionchangesenatorstatus.php
+++ actions/actionchangesenatorstatus.php
@@ -9,6 +9,7 @@
 <?php
 // TODO: Post Field Verification
 $personscannerid= $_POST["useremail"];
+$newstatus= $_POST["newstatus"];
 ini_set('max_execution_time', 300);
                     require '../mysqlkeys.php';
                     $conn = new mysqli($host, $user, $password, $dbname);
@@ -16,9 +17,10 @@ ini_set('max_execution_time', 300);
     die("Connection failed: " . $conn->connect_error);
 }
 
-            $stmt = $conn->prepare("UPDATE `ppv0008003`.`studentlisting` SET `Senator` = 0 WHERE `EmailAddress` = ?");
-            $stmt->bind_param("s", $field1);
-            $field1=$personscannerid;
+            $stmt = $conn->prepare("UPDATE `ppv0008003`.`studentlisting` SET `Senator` = ? WHERE `EmailAddress` = ?");
+            $stmt->bind_param("is", $field1, $field2);
+            $field1=$newstatus;
+            $field2=$personscannerid;
             $stmt->execute();
 ?>
 <h2>Success User Status Changed!</h2></div>
