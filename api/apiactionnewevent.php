b707ccbc291deb1bc3b250c2154b9b25b4daec27
Ryan Evaul <ryan.evaul@snhu.edu>
2019-09-08 13:56:19 -0400

    Fix API1

--- api/apiactionnewevent.php
+++ api/apiactionnewevent.php
@@ -1,17 +1,9 @@
-<?php require "../login/loginheader.php"; ?>
-<?php require "../login/permissions/level3.php"; ?>
-<?php include "../template/top.php"; ?>
-<?php require '../mysqlkeys.php'; ?>
-<div id="about" class="container-fluid">
-      <div class="row">
-              <div class="col-sm-1">
-    </div>
-    <div class="col-sm-10">
+
 <?php
 // TODO: Post Field Verification
 $eventname= $_POST["name"];
 $eventloc= $_POST["location"];
-$eventtype= $_POST["eventtype"];
+$eventtypemask= $_POST["eventtypemask"];
 $eventhost= $_POST["host"];
 $eventpoints= $_POST["pointvalue"];
 $eventdoublepoints= $_POST["doublepoints"];
@@ -56,9 +48,3 @@ $stmt->close();
 $conn->close();
 
 ?>
-<h2>Success Event Added!</h2></div>
-    <div class="col-sm-1">
-    </div>
-  </div>
-</div>
-<?php include "../template/bottom.php" ?>
