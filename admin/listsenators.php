61954091e92cc8e1079fac1eb5660f430b039403
Ryan Evaul <ryan.evaul@snhu.edu>
2019-09-08 11:34:17 -0400

    Fix Email

--- admin/listsenators.php
+++ admin/listsenators.php
@@ -48,7 +48,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-       ?><tr><td> <?php echo $row["email"];
+       ?><tr><td> <?php echo $row["emailaddress"];
 	   ?></td><td><?php echo $row["StudentID"];
 	   ?></td><td><?php echo $row["StudentLastName"];
 	   ?></td><td><?php echo $row["StudentFirstName"];
