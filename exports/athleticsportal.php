0996fe9ec227252065419a24bbcf1c1cacbca187
Ryan Evaul <ryan.evaul@snhu.edu>
2019-09-19 12:20:28 -0400

    Athletics Portal

---
+++ exports/athleticsportal.php
@@ -0,0 +1,16 @@
+<?php require "../login/loginheader.php"; ?>
+<?php require "../login/permissions/level4.php"; ?>
+<?php include "../template/top.php"; ?>
+<div id="about" class="container-fluid">
+      <div class="row">
+              <div class="col-sm-1">
+    </div>
+    <div class="col-sm-10">
+        <h2>Campus Recreation Data Portal</h2>
+<iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=581a8b45-7104-4f06-b4df-0e85a141e45b&autoAuth=true&ctid=a8eec281-aaa3-4dae-ac9b-9a398b9215e7&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXVzLW5vcnRoLWNlbnRyYWwtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+        </div>
+    <div class="col-sm-1">
+    </div>
+  </div>
+</div>
+<?php include "../template/bottom.php" ?>
