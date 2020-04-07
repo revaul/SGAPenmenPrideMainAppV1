712bbd78bacf9c1cfdb28f63506a60fea448216a
Ryan Evaul <ryan.evaul@snhu.edu>
2020-04-07 16:17:42 -0400

    Keys Import

--- emailkeys.php
+++ emailkeys.php
@@ -14,17 +14,24 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
-       echo $row["epw"];
-       echo $row["ea"];
-       echo $row["efn"];
-       echo $row["eh"];
-       echo $row["es"];
-       echo $row["ep"];
-       echo $row["ef"];
+       $_SESSION['mailfrom']= $row["ef"];
+$_SESSION['mailhost']= $row["eh"];
+$_SESSION['mailport']= $row["ep"];
+$_SESSION['mailnamefrom']= $row["efn"];
+$_SESSION['mailusername']= $row["ea"];
+$_SESSION['mailpassword']= $row["epw"];
+$_SESSION['mailsecurity']= $row["es"];
+
     }
 } else {
 }
 $conn->close();
 
+echo $_SESSION['mailhost'];
+echo $_SESSION['mailport'];
+echo $_SESSION['mailnamefrom'];
+echo $_SESSION['mailusername'];
+echo $_SESSION['mailpassword'];
+echo $_SESSION['mailsecurity'];
 //}
 ?>
