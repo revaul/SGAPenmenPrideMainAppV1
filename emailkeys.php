9fc0446f3eb57a1e8a0f9890a6ab00907639c417
Ryan Evaul <ryan.evaul@snhu.edu>
2020-04-07 16:11:22 -0400

    Fix Query

--- emailkeys.php
+++ emailkeys.php
@@ -8,7 +8,7 @@ $conn = new mysqli($host, $user, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT op1.optionvalue epw, op2.optionvalue ea, op3.optionvalue efn,  op4.optionvalue eh,  op5.optionvalue es,  op6.optionvalue ep,  op7.optionvalue ef  FROM ppv0008003.options op1 left join ppv0008003.options op2 on 1=1 and op2.optiondesc='Email Address' and op2.optionsgroup=1 left join ppv0008003.options op3 on 1=1 and op3.optiondesc='Email From Name' and op3.optionsgroup=1 left join ppv0008003.options op4 on 1=1 and op4.optiondesc='Email Host' and op4.optionsgroup=1 left join ppv0008003.options op5 on 1=1 and op5.optiondesc='Email Security' and op5.optionsgroup=1 left join ppv0008003.options op6 on 1=1 and op6.optiondesc='Email Port' and op6.optionsgroup=1 left join ppv0008003.options op7 on 1=1 and op7.optiondesc='Email From' and op7.optionsgroup=1 where op1.optiondesc='Email Password' and op1.optionsgroup=1;";
+$sql = "select "SELECT * FROM ppv0008003.EmailLoginInfo;";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
