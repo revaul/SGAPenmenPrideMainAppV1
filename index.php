439136250223043beecea59aa529b3ffba765f56
Ryan Evaul <ryan.evaul@snhu.edu>
2020-04-07 16:29:11 -0400

    Maintenance

--- index.php
+++ index.php
@@ -1,52 +1,18 @@
-<?php require "login/loginheader.php"; ?>
-<?php require "login/permissions/level1.php"; ?>
-<?php
-/*if(isset($_SESSION['originaladdress'])){
- $newoldurlforredirect = $_SESSION['originaladdress'];
- unset($_SESSION['originaladdress']);
- if($newoldurlforredirect!='/'){
-//return header("location:".$newoldurlforredirect);
-}
-}*/
-?>
-<?php include "template/top.php"; ?>
-<?php include "template/jumbotron.php"; ?>
-<div id="about" class="container-fluid">
-  <div class="row">
-    <div class="col-sm-8">
-      <!-- IDEA: Change the text on the index to a status dashboard. -->
-      <h2>Welcome <?php echo $_SESSION['first']; ?>,</h2>
-      <h3>Your last login was: <?php
-      require 'mysqlkeys.php';
-      // Create connection
-      $conn = new mysqli($host, $user, $password, $dbname);
-      // Check connection
-      if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-      }
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
 
-
-      $sql = "SELECT * FROM loginsystem.LastLogin where username='" . $_SESSION['username'] . "';";
-      $result = $conn->query($sql);
-
-      if ($result->num_rows > 0) {
-          // output data of each row
-          while($row = $result->fetch_assoc()) {
-              echo $row["td"];
-          }
-      } else {
-
-      }
-      $conn->close();
-
-       ?></h3>
-      <h4>The Penmen Pride program is an initiative that has SNHU students enthusiastic about participating in events and programs around campus. This program enhances school spirit within our student body by implementing incentives for students to attend events. By having your event be a Penmen Pride event, you can look forward to students showing up excited to attend. Penmen Pride is just one of the ways that the Student Government Association builds student community spirit by creating a common goal among all SNHU students - to earn points, and win prizes!  </h4><br>
-
-
+<article>
+    <h1>We&rsquo;ll be back soon!</h1>
+    <div>
+        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+        <p>&mdash; The Team</p>
     </div>
-    <div class="col-sm-4">
-      <span class="glyphicon glyphicon-signal logo"></span>
-    </div>
-  </div>
-</div>
-<?php include "template/bottom.php" ?>
+</article>
