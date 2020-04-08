ae900d3a0dd167d09d5d8bcf78bea9917d92534d
Ryan Evaul <ryan.evaul@snhu.edu>
2020-04-08 11:32:07 -0400

    testing

--- login/loginheader.php
+++ login/loginheader.php
@@ -1,8 +1,13 @@
 <?php
-/*header("Location: http://penmenpride.snhusga.org/maintenance.php");
-exit();*/
-//PUT THIS HEADER ON TOP OF EACH UNIQUE PAGE
 session_start();
+
+//PUT THIS HEADER ON TOP OF EACH UNIQUE PAGE
+if($_SESSION['username']=='rce423'){
+}
+else{
+  header("Location: http://penmenpride.snhusga.org/maintenance.php");
+  exit();
+}
 $_SESSION['originaladdress']=$_SERVER['REQUEST_URI'];
 if (!isset($_SESSION['username'])) {
   $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
