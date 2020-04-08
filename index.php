6ac6a1c6e2a0d92fc2a8db0b66e69615ee63a616
Ryan Evaul <ryan.evaul@snhu.edu>
2020-04-07 20:33:39 -0400

    Email keys

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 //return header("location:".$newoldurlforredirect);
 }
 }*/
-
+require "emailkeys.php";
 ?>
 <?php include "template/top.php"; ?>
 <?php include "template/jumbotron.php"; ?>
@@ -41,10 +41,7 @@
       $conn->close();
 
        ?></h3>
-      <h4>The Penmen Pride program is an initiative that has SNHU students enthusiastic about participating in events and programs around campus. This program enhances school spirit within our student body by implementing incentives for students to attend events. By having your event be a Penmen Pride event, you can look forward to students showing up excited to attend. Penmen Pride is just one of the ways that the Student Government Association builds student community spirit by creating a common goal among all SNHU students - to earn points, and win prizes!<?php echo $mailfrom;
-  echo  $_SESSION['mailfrom'];
-      echo $mailhost;
-       ?>  </h4><br>
+      <h4>The Penmen Pride program is an initiative that has SNHU students enthusiastic about participating in events and programs around campus. This program enhances school spirit within our student body by implementing incentives for students to attend events. By having your event be a Penmen Pride event, you can look forward to students showing up excited to attend. Penmen Pride is just one of the ways that the Student Government Association builds student community spirit by creating a common goal among all SNHU students - to earn points, and win prizes!  </h4><br>
 
 
     </div>
