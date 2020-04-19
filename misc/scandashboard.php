0d711a4a19f7c9e4d113de0e3bf73bffb03634d1
Ryan Evaul <ryan.evaul@snhu.edu>
2020-04-18 22:26:31 -0400

    Appending Completion

--- misc/scandashboard.php
+++ misc/scandashboard.php
@@ -42,6 +42,21 @@ $c++;
 } else {
 
 }
+if($c==2){
+  <div class="col-sm-3">blah</div>
+  <div class="col-sm-3">Blah</div>
+  <div class="col-sm-3">Blah</div>
+  </div>
+}
+elseif($c==3){
+  <div class="col-sm-3">Blah</div>
+  <div class="col-sm-3">Blah</div>
+  </div>
+}
+elseif($c==4){
+  <div class="col-sm-3">Blah</div>
+  </div>
+}
 $conn->close();
 ?>
 
